bad7cc630b6d591f41c2f179e8ce3f8a08d640c9
wesleyweng <wesleyweng@nineder.com>
2023-09-22 09:17:19 +0800

    feat: add Vue3.3 new feature in config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      script: {
+        defineModel: true,
+        propsDestructure: true
+      }
+    }),
   ],
   resolve: {
     alias: {
