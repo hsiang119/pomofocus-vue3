c0b47f6694770e146555536afa74cf3a13682a04
wesleyweng <wesleyweng@nineder.com>
2023-09-22 09:14:02 +0800

    style: modify tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,51 @@ module.exports = {
   ],
   theme: {
     extend: {
+      strokeDashoffset: {
+        0: '0',
+        100: '100',
+      },
       colors: {
         background: '#06a4ed',
-        primary: '#2dbdff',
+        primary: "#06b6d4",
         secondary: '#F6FAFB',
         accent: '#F37F15',
         black: '#040F15'
       },
       maxWidth: {
-        '1200': '1200px'
+        '1200': '1200px',
+        '1000': '1000px',
+        '600': '600px',
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in both',
+				fadeDownIn: 'fadeDownIn 0.5s ease-in both',
+				fadeLeftIn: 'fadeLeftIn 0.5s ease-in both',
+        letterMove: 'letterMove 3s infinite alternate ease-in-out'
+      },
+      keyframes: {
+        fadeIn: {
+					'0%': { opacity: '0' },
+					'100%': { opacity: '1' },
+				},
+				fadeDownIn: {
+					'0%': { opacity: '0', transform: 'translateY(100px)' },
+					'100%': { opacity: '1', transform: 'translateY(0)' },
+				},
+				fadeLeftIn: {
+					'0%': { opacity: '0', transform: 'translateX(-100px)' },
+					'100%': { opacity: '1', transform: 'translateX(0)' },
+				},
+        letterMove: {
+          from: { letterSpacing: '-2rem', filter: 'blur(20px)' },
+          to: { letterSpacing: '1rem', filter: 'blur(0)' },
+        }
+      },
+      zIndex: {
+				'-1': '-1',
+			},
+      borderRadius: {
+        default: '10px'
       }
     },
   },
