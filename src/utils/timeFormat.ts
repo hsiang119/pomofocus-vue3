bcf229f44d4568160cc48bb3c1cd3d3aa87c8108
wesleyweng <wesleyweng@nineder.com>
2023-09-22 10:50:12 +0800

    feat: add utils about time format

---
+++ src/utils/timeFormat.ts
@@ -0,0 +1,5 @@
+export function onGetTime(distance: number): string {
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    return `${minutes}:${seconds}`;
+}
