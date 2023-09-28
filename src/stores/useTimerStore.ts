164ed9274a25b45581b8615ce6557a9ba5dbd804
wesleyweng <wesleyweng@nineder.com>
2023-09-28 16:00:08 +0800

    feat: modify useTimerStore

--- src/stores/useTimerStore.ts
+++ src/stores/useTimerStore.ts
@@ -1,29 +1,14 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
-
-const enum CountdownMode {
-  Pomodoro = 'POMODORO',
-  LongBreak = 'LONG_BREAK',
-  ShortBreak = 'SHORT_BREAK'
-}
+import { CountdownMode } from "../types";
 
 export const useTimerStore = defineStore('timer', () => {
-  // State
-  const mode = ref(CountdownMode.Pomodoro);
+  const mode = ref(CountdownMode.pomodoro);
   const isActive = ref<boolean>(false);
 
-  // Getters
-  const getMode = computed(() => mode.value)
-  const getStatus = computed(() => isActive.value)
-
-  // Actions
-  function changeTimeStatus(timeMode: string): void {
-    mode.value = timeMode as CountdownMode
-  }
-
-  function changeActiveStatus():void {
+  function changeActiveStatus(): void {
     isActive.value = !isActive.value
-  }
- 
-  return { mode, isActive, getMode, getStatus, changeTimeStatus, changeActiveStatus }
+  };
+
+  return { mode, isActive, changeActiveStatus }
 })
