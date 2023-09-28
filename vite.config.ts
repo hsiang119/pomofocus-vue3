a3f754ff151cc00f8242ff4e69738aabfefcf30d
wesleyweng <wesleyweng@nineder.com>
2023-09-28 14:48:24 +0800

    feat: modify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
+  build: {
+    outDir: 'doc',
+  },
   plugins: [
     vue({
       script: {
