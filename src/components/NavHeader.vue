0c49f496c3b69f4b3e240d2f98967e3929776fca
wesleyweng <wesleyweng@nineder.com>
2023-09-29 15:21:36 +0800

    feat: modify NavHeader style and logic

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,26 +1,15 @@
-<template>
-    <header class="flex gap-2 justify-around h-20 w-full mb-[30px]">
-        <h1 class="leading-loose max-w-prose">
-            <RouterLink to="/" >Pomofocus</RouterLink>
-        </h1>
-        
-        <nav class="">
-            <ul class="flex">
-                <li v-for="item in NAV_ITEMS" :key="item.id" class="ml-5 min-h-full transition-transform transition-shadow duration-300 hover:translate-y-[2px] active:translate-y-[5px]">
-                    <RouterLink to="/login" class="text-base leading-loose max-w-prose nav-item ">{{ item.name }}</RouterLink>
-                </li>
-            </ul>
-        </nav>
-    </header>
-</template>
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
 
-<script setup>
-import { ref } from "vue";
+interface NavItem {
+  id: string;
+  name: string;
+}
 
-const NAV_ITEMS = ref([
+const NAV_ITEMS: Ref<NavItem[]> = ref<NavItem[]>([
   {
     id: 'item01',
-    name: 'shared'
+    name: 'analysis'
   },
   {
     id: 'item02',
@@ -32,9 +21,24 @@ const NAV_ITEMS = ref([
   },
 ])
 
-
-
 </script>
+
+<template>
+  <header class="flex justify-between h-20 w-[97%] mb-[30px]">
+
+      <h1 class="leading-loose contrast-20 w-[100px] text-center">
+          <RouterLink to="/" class="animate-letterMove whitespace-nowrap">Pomofocus</RouterLink>
+      </h1>
+      
+      <nav class="animate-fadeIn flex items-center gap-2">
+          <ul class="flex">
+              <li v-for="item in NAV_ITEMS" :key="item.id" class="leading-loose ml-5 min-h-full transition-transform transition-shadow duration-300 hover:translate-y-[2px] active:translate-y-[5px]">
+                  <RouterLink to="/login" class="text-base max-w-prose nav-item ">{{ item.name }}</RouterLink>
+              </li>
+          </ul>
+      </nav>
+  </header>
+</template>
 
 <style scoped>
     
