f05da59061e883dd6a6e93fa17f29c153ec3a6ea
wesleyweng <wesleyweng@nineder.com>
2023-09-29 15:17:40 +0800

    feat: add new util function

---
+++ src/utils/common.ts
@@ -0,0 +1,3 @@
+export function containsWhitespace(str: string) {
+    return /\s/.test(str);
+}
--- src/utils/timeFormat.ts
+++ src/utils/timeFormat.ts
@@ -1,5 +1,6 @@
 export function onGetTime(distance: number): string {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    return `${minutes}:${seconds}`;
+    
+    return `${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
 }
