0dc9aac351142e3e04f5f6ce611471289e54f30d
wesleyweng <wesleyweng@nineder.com>
2023-10-19 19:51:02 +0800

    style: modify tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
       },
       backgroundImage: {
         'custom-gradient': 'linear-gradient(0deg, transparent, transparent, #45f3ff, #45f3ff, #45f3ff)',
+        'under-background': 'linear-gradient(45deg, #081b29, "#0ef")',
       },
       contrast: {
         20: '20',
@@ -76,12 +77,36 @@ module.exports = {
       },
       boxShadow: {
         'burger' : '0px 2px 5px rgba(255,101,47,.2)',
-        'activeButton': '0 0 5px #45f3ff'
+        'activeButton': '0 0 5px #45f3ff',
+        'neon': `inset 0 0 .5em #0ef, 0 0 .5em #0ef`,
+        'custom-cyan': '0 0 25px #0ef',
+      },
+      transitionDelay: {
+        '400': '400ms',
+        '550': '550ms',
+        '600': '600ms',
+        '650': '650ms',
+        '1100': '1100ms',
+        '1200': '1200ms',
+        '1300': '1300ms',
+        '1400': '1400ms',
+        '1500': '1500ms',
+        '1600': '1600ms',
+        '1700': '1700ms',
+        '1800': '1800ms',
+        '1900': '1900ms',
+        '2000': '2000ms',
+        '2100': '2100ms',
+        '2200': '2200ms',
+        '2300': '2300ms',
+        '2400': '2400ms',
+        '2500': '2500ms',
+        '2600': '2600ms',
       }
     },
   },
   plugins: [
-    plugin(({ addUtilities, theme }) => {
+    plugin(({ addUtilities, theme, addVariant }) => {
       const newUtilities = {
         '.flex-center': {
           'display': 'flex',
@@ -111,6 +136,14 @@ module.exports = {
         } 
       };
       addUtilities(newUtilities);
+      addVariant('important', ({ container }) => {
+        container.walkRules((rule) => {
+          rule.selector = `.\\!${rule.selector.slice(1)}\\`;
+          rule.walkDecls((decl) => {
+            decl.important = true;
+          });
+        });
+      });
     }),
     
   ],
