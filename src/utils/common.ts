92526e8d584334015f24f79e594bc26ad8e6e65f
wesleyweng <wesleyweng@nineder.com>
2023-10-19 19:53:31 +0800

    feat: add common utils

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,8 @@
 export function containsWhitespace(str: string) {
     return /\s/.test(str);
 }
+
+export function validateEmail(email: string) {
+    const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return pattern.test(email);
+}
