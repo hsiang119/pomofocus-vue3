d3e4ee57384aa19031ae98693cb196c276c0e190
wesleyweng <wesleyweng@nineder.com>
2023-10-19 15:51:54 +0800

    style: modify timer width

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import NavHeader from "./components/NavHeader.vue";
 </script>
 
 <template>
-  <main class="xl:max-w-1200 lg:max-w-1000 md:max-w-600 w-full mx-auto my-0 h-screen">
-    <NavHeader/>
+  <main class="xl:max-w-1200 lg:max-w-1000 md:max-w-600 w-[90vw] mx-auto my-0 h-screen">
+    <NavHeader />
     <RouterView/>
   </main>
 </template>
