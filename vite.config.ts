d40622139c77eeba166eb3de9ce06e8301312bae
wesleyweng <wesleyweng@nineder.com>
2023-10-20 14:11:27 +0800

    fix: change history mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pomofocus-vue3',
+  // base: '/pomofocus-vue3',
   plugins: [
     vue({
       script: {
