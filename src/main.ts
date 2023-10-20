7e1055b352b50c89ec4d09c0c5be5c65b5e1dc23
wesleyweng <wesleyweng@nineder.com>
2023-10-20 10:46:28 +0800

    style: add fontawesome

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,23 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret, faGoogle)
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.component('font-awesome-icon', FontAwesomeIcon).mount('#app')
