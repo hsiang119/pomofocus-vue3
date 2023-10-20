efc71f936f1413fdd0143f8eb4f8d3e8d27d4342
wesleyweng <wesleyweng@nineder.com>
2023-10-20 14:41:05 +0800

    fix: fix route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginPage from "@/views/LoginPage.vue";
 import AnalysisReport from "@/views/AnalysisReport.vue";
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 const router = createRouter({
+  // history: createWebHistory(import.meta.env.BASE_URL),
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/pomofocus-vue3/',
-  // base: './',
+  // base: '/',
   plugins: [
     vue({
       script: {
