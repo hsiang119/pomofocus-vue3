721471851e7bf633309668052d1b523330a39a1b
wesleyweng <wesleyweng@nineder.com>
2023-10-21 17:18:14 +0800

    feat: add notfound page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    import { useRouter } from "vue-router";
+
+    const router = useRouter();
+</script>
+
+<template>
+    <main class="flex justify-center items-center flex-col w-[600px] h-[300px] my-0 mx-auto">
+        <h2 class="text-5xl text-center mt-10">
+            Oops! 404 Page not found.
+        </h2>
+        <button @click="() => router.push('/')" class="btn w-[180px] hover:text-light border-white text-base font-medium py-[10xp] px-[25px] m-auto leading-10 hover:shadow-activeButton transition-all duration-300 ease-in hover:">
+            Back to home
+        </button>
+    </main>
+</template>
+
+<style scoped>
+
+</style>
