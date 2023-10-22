85f886edf3bca05cb7f70dcfa1b971851956053e
wesleyweng <wesleyweng@nineder.com>
2023-10-22 21:00:10 +0800

    feat: add utils function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -6,3 +6,7 @@ export function validateEmail(email: string) {
     const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     return pattern.test(email);
 }
+
+export function validatePasswordCharacters(password: string) {
+    return password.length >= 6
+}
