8918e22ac43e580d8c9897ab2e64be3c212e6f49
wesleyweng <wesleyweng@nineder.com>
2023-10-22 20:57:45 +0800

    feat: add toast component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <script setup lang="ts">
+import { markRaw, onMounted } from "vue";
 import { RouterView } from 'vue-router'
 import NavHeader from "./components/NavHeader.vue";
+import GlobalToast from "@/components/GlobalToast.vue";
+import useGlobalToast from "@/composables/useGlobalToast";
+
+const toast = useGlobalToast();
+
+onMounted( () => {
+  toast.component.value = markRaw(GlobalToast);
+})
 
 </script>
 
 <template>
+  <GlobalToast @close="() => toast.closeToast()" :show="toast.show.value" :message="toast.message.value" :type="toast.type.value" :display-time="toast.time.value" />
+
   <main class="xl:max-w-1200 lg:max-w-1000 md:max-w-600 w-[90vw] mx-auto my-0 h-screen">
     <NavHeader />
-    <RouterView/>
+    <transition name="fade" mode="out-in">
+      <RouterView/>
+    </transition>
   </main>
 </template>
 
 <style scoped>
-
+  .fade-enter-active, .fade-leave-active {
+        transition: opacity 1s;
+    }
+    .fade-enter-from, .fade-leave-to {
+        opacity: 0;
+    }
+    .fade-enter-to, .fade-leave-from {
+        opacity: 1;
+    }
 </style>
